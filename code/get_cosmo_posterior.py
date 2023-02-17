831e8f436359ff160c5b388318bcd7c81c6e56af
emilleishida <emilleishida@gmail.com>
2023-02-17 22:33:20 +0100

    add doc

--- code/get_cosmo_posterior.py
+++ code/get_cosmo_posterior.py
@@ -1,3 +1,23 @@
+# Copyright 2023 resspect software
+# Author: Emille E. O. Ishida
+# 
+# slightly modified from Malz et al., 2023
+# https://github.com/COINtoolbox/RESSPECT_metric/blob/main/code/get_cosmo_posterior.py
+#
+# created on 17 January 2023
+#
+# Licensed MIT License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://opensource.org/license/mit/
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pandas as pd
 import numpy as np
 import pystan
